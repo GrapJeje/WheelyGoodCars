21b9794f67305da423347fdcc6f11c2313470b5b
Jason <jasonvanl2019@gmail.com>
2026-02-06 16:46:02 +0100

    Added a basic form for the add car listing system

---
+++ resources/views/livewire/listings/create.blade.php
@@ -0,0 +1,23 @@
+<?php
+
+use Livewire\Attributes\Layout;
+use Livewire\Volt\Component;
+
+new #[Layout('layouts.default')]
+class extends Component {
+    //
+}; ?>
+
+<div>
+    <form action="">
+        <div>
+            <p>NL</p>
+        </div>
+        <div>
+            <input title="text" placeholder="nummerbord">
+        </div>
+        <div>
+            <button type="submit">GO</button>
+        </div>
+    </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Livewire\Volt\Volt;
 Volt::route('/', 'home')
     ->name('home');
 
+Volt::route('/add', 'listings.create')
+    ->name('add.car');
+
 Route::middleware('guest')->group(function () {
     Volt::route('/register', 'auth.register')
         ->name('register');
