5bf4e240caa9ceffaa8c546bc57c115051c812a9
Jason <jasonvanl2019@gmail.com>
2026-02-12 16:51:44 +0100

    Added a container to the header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,12 @@
 <header>
-    <a href="{{ route('home') }}" class="logo">WheelyGoodCars!</a>
+    <div class="container">
+        <div class="header">
+            <a href="{{ route('home') }}" class="logo">WheelyGoodCars!</a>
 
-    <div>
-        <a href="{{ route('add.car') }}" class="add-car">Plaats aanbod</a>
-        <a href="{{ route('login') }}" class="login">Inloggen</a>
+            <div>
+                <a href="{{ route('add.car') }}" class="add-car">Plaats aanbod</a>
+                <a href="{{ route('login') }}" class="login">Inloggen</a>
+            </div>
+        </div>
     </div>
 </header>
